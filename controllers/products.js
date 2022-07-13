12f31f370e81173c3b4d39ce6593489f4674cae1
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-12 22:37:18 -0700

    Add find by name and sort functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,34 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({
-    name: 'vase table',
-  })
-  res.status(200).json({products, nbHits: products.length})
+  const search = 'ab'
+  const products = await Product.find({}).sort('-name price')
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query)
-  res.status(200).json({products, nbHits: products.length})
+  const { featured, company, name, sort } = req.query
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = { $regex: search, $options: 'i' }
+  }
+  console.log(queryObject)
+  let result = Product.find(queryObject).sort()
+  if (sort) {
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createdAt')
+  }
+  const products = await result
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
